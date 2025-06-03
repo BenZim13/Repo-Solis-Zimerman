aac91f47fb26e8e7c15e56920b40ff387a07c5c2
Benjamin Zimerman <benjaminzimermanxiaomi@gmail.com>
2025-06-03 19:39:18 -0300

    BASE DATOS

--- bd_solis_agustina_zimerman_benjamin.sql
+++ bd_solis_agustina_zimerman_benjamin.sql
@@ -3,11 +3,10 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 03-06-2025 a las 15:09:40
+-- Tiempo de generación: 04-06-2025 a las 00:36:03
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
-SET FOREIGN_KEY_CHECKS=0;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
@@ -29,10 +28,12 @@ SET time_zone = "+00:00";
 --
 
 DROP TABLE IF EXISTS `categoria`;
-CREATE TABLE `categoria` (
-  `id_categoria` int(50) UNSIGNED NOT NULL,
-  `nombre` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE IF NOT EXISTS `categoria` (
+  `id_categoria` int(50) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(20) NOT NULL,
+  PRIMARY KEY (`id_categoria`),
+  UNIQUE KEY `Category` (`nombre`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `categoria`
@@ -42,10 +43,10 @@ INSERT INTO `categoria` (`id_categoria`, `nombre`) VALUES
 (5, 'Alimentos'),
 (7, 'Bebidas'),
 (3, 'Carniceria'),
-(1, 'Fiambrería'),
+(1, 'Fiambreria'),
 (4, 'Frutas y Verduras'),
 (6, 'Limpieza'),
-(2, 'Panadería');
+(2, 'Panaderia');
 
 -- --------------------------------------------------------
 
@@ -54,61 +55,28 @@ INSERT INTO `categoria` (`id_categoria`, `nombre`) VALUES
 --
 
 DROP TABLE IF EXISTS `producto`;
-CREATE TABLE `producto` (
-  `id_producto` int(50) NOT NULL,
+CREATE TABLE IF NOT EXISTS `producto` (
+  `id_producto` int(50) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(255) NOT NULL,
   `descripcion` text DEFAULT NULL,
   `precio` decimal(10,2) NOT NULL,
   `stock` int(50) NOT NULL DEFAULT 0,
   `image_url` varchar(255) DEFAULT NULL,
   `activo` tinyint(1) NOT NULL DEFAULT 1,
-  `fecha_alta` datetime NOT NULL,
-  `fecha_modifica` datetime NOT NULL,
+  `fecha_alta` datetime NOT NULL DEFAULT current_timestamp(),
+  `fecha_modifica` datetime NOT NULL DEFAULT current_timestamp(),
   `fecha_elimina` datetime DEFAULT NULL,
-  `id_categoria` int(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `id_categoria` int(11) NOT NULL,
+  PRIMARY KEY (`id_producto`),
+  KEY `idx_productos_id_categoria` (`id_categoria`)
+) ENGINE=InnoDB AUTO_INCREMENT=1235 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `producto`
 --
 
 INSERT INTO `producto` (`id_producto`, `nombre`, `descripcion`, `precio`, `stock`, `image_url`, `activo`, `fecha_alta`, `fecha_modifica`, `fecha_elimina`, `id_categoria`) VALUES
-(1234, 'Fideo Tirabuzón Favorita', 'Fideos por 500gr', 300.00, 4, NULL, 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00', NULL, 2);
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `categoria`
---
-ALTER TABLE `categoria`
-  ADD PRIMARY KEY (`id_categoria`),
-  ADD UNIQUE KEY `Category` (`nombre`);
-
---
--- Indices de la tabla `producto`
---
-ALTER TABLE `producto`
-  ADD PRIMARY KEY (`id_producto`),
-  ADD KEY `idx_productos_id_categoria` (`id_categoria`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `categoria`
---
-ALTER TABLE `categoria`
-  MODIFY `id_categoria` int(50) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT de la tabla `producto`
---
-ALTER TABLE `producto`
-  MODIFY `id_producto` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1235;
-SET FOREIGN_KEY_CHECKS=1;
+(1234, 'Fideos Favorita', ' Spaguetti 500 gr', 400.00, 5, NULL, 1, '2025-06-03 11:41:14', '2025-06-03 11:41:14', NULL, 5);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
