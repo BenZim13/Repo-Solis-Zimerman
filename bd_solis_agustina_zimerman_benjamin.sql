02bd97f7581d60a95fbbcee3e94b46b751614b23
Benjamin Zimerman <benjaminzimermanxiaomi@gmail.com>
2025-06-22 21:00:47 -0300

    22/6

--- bd_solis_agustina_zimerman_benjamin.sql
+++ bd_solis_agustina_zimerman_benjamin.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 11-06-2025 a las 00:22:57
+-- Tiempo de generación: 23-06-2025 a las 01:59:41
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -20,6 +20,20 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `bd_solis_agustina_zimerman_benjamin`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `carrito`
+--
+
+CREATE TABLE `carrito` (
+  `id_carrito` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_producto` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `fecha_agregado` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -44,6 +58,30 @@ INSERT INTO `categoria` (`id_categoria`, `nombre`) VALUES
 (4, 'Frutas y Verduras'),
 (6, 'Limpieza'),
 (2, 'Panaderia');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `version` varchar(255) NOT NULL,
+  `class` varchar(255) NOT NULL,
+  `group` varchar(255) NOT NULL,
+  `namespace` varchar(255) NOT NULL,
+  `time` int(11) NOT NULL,
+  `batch` int(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `version`, `class`, `group`, `namespace`, `time`, `batch`) VALUES
+(1, '2025-06-21-001028', 'App\\Database\\Migrations\\AddRolToUsuariosTable', 'default', 'App', 1750464847, 1),
+(2, '2025-06-21-002716', 'App\\Database\\Migrations\\AddActivoToUsuariosTable', 'default', 'App', 1750465781, 2);
 
 -- --------------------------------------------------------
 
@@ -74,12 +112,45 @@ INSERT INTO `producto` (`id_producto`, `nombre`, `descripcion`, `precio`, `stock
 (1234, 'Fideos Favorita', ' Spaguetti 500 gr', 400.00, 5, NULL, 1, '2025-06-03 11:41:14', '2025-06-09 20:47:58', '2025-06-09 20:47:58', 5),
 (1235, 'Gaseosa Coca Cola', '1 litro sabor cola', 3500.00, 3, '', 1, '2025-06-09 20:33:41', '2025-06-09 20:36:29', '2025-06-09 20:36:29', 7),
 (1236, 'Gaseosa Coca Cola', '1 litro', 6000.00, 20, '', 1, '2025-06-09 20:46:59', '2025-06-09 20:47:26', '2025-06-09 20:47:26', 7),
-(1237, 'Gaseosa Coca Cola', '1 litro', 6000.00, 20, '', 1, '2025-06-09 21:21:11', '2025-06-09 21:21:11', NULL, 7),
-(1238, 'Pan', 'Lactal', 1200.00, 10, '', 1, '2025-06-09 21:21:58', '2025-06-09 21:21:58', NULL, 2);
+(1237, 'Gaseosa Coca Cola', '1 litro', 6000.00, 12, '', 1, '2025-06-09 21:21:11', '2025-06-22 23:30:04', NULL, 7),
+(1238, 'Pan', 'Lactal', 1200.00, 3, '', 1, '2025-06-09 21:21:58', '2025-06-22 23:30:04', NULL, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `rol` int(5) NOT NULL DEFAULT 2,
+  `activo` tinyint(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_usuario`, `nombre`, `email`, `password`, `rol`, `activo`) VALUES
+(1, 'Benjamin', 'benja@gmail.com', '$2y$10$953ss/R/PHgD6PMxHOPJ3OqboKza9BgyKhqp7WWdVy6GFwH/CwY0e', 2, 1),
+(2, 'Agustin', 'agus@gmail.com', '$2y$10$VZwHMBe2U5xgo3alACF2zOLx8iJJ5RiSHL8Lnf.8cn9jsV.ZCm9j.', 2, 1),
+(3, 'marta', 'marta@gmail.com', '$2y$10$RmIMcvl/yYcENNT/6/4IS.N123yk/5SX9PX5e8ooqRAjfKGEh6Jay', 2, 1),
+(4, 'tobias', 'tobias@gmail.com', '$2y$10$TSdcXeny9Zfs/BCtoQF.6uPUp92Ah.HRghDPJtoklbAjBKttwNybO', 2, 1);
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `carrito`
+--
+ALTER TABLE `carrito`
+  ADD PRIMARY KEY (`id_carrito`),
+  ADD KEY `id_usuario` (`id_usuario`),
+  ADD KEY `id_producto` (`id_producto`);
 
 --
 -- Indices de la tabla `categoria`
@@ -89,6 +160,12 @@ ALTER TABLE `categoria`
   ADD UNIQUE KEY `Category` (`nombre`);
 
 --
+-- Indices de la tabla `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indices de la tabla `producto`
 --
 ALTER TABLE `producto`
@@ -96,8 +173,21 @@ ALTER TABLE `producto`
   ADD KEY `idx_productos_id_categoria` (`id_categoria`);
 
 --
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `carrito`
+--
+ALTER TABLE `carrito`
+  MODIFY `id_carrito` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT de la tabla `categoria`
@@ -106,10 +196,39 @@ ALTER TABLE `categoria`
   MODIFY `id_categoria` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
+-- AUTO_INCREMENT de la tabla `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT de la tabla `producto`
 --
 ALTER TABLE `producto`
   MODIFY `id_producto` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1239;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `carrito`
+--
+ALTER TABLE `carrito`
+  ADD CONSTRAINT `carrito_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`),
+  ADD CONSTRAINT `carrito_ibfk_2` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id_producto`);
+
+--
+-- Filtros para la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD CONSTRAINT `fk_producto_categoria` FOREIGN KEY (`id_categoria`) REFERENCES `categoria` (`id_categoria`) ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
